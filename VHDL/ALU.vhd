c4be9d1c7c9c4fd8f72352d68f5c5fa1674f64f5
pbwwwwwwwwbp <jimmerwearpurple@gmail.com>
2015-03-22 00:43:30 +0800

    update ALU overflow
    delete unsigned arithmetic and add less output to logic operation

--- VHDL/ALU.vhd
+++ VHDL/ALU.vhd
@@ -24,41 +24,37 @@ begin
           msb_a := A(31) and B(31);
           msb_b := A(30) and B(30);
           less <= msb_a xor msb_b;
-        when "0001" =>
-          result <= (unsigned)A + (unsigned)B;
-          less <= A(31) and B(31);
         when "0010" =>
           result <= A + (not B) + 1;
           msb_a := A(31) and B(31);
           msb_b := A(30) and B(30);
           less <= msb_a xor msb_b;
-        when "0011" =>
-          result <= (unsigned)A + (not (unsigned)B) + 1;
-          if (unsigned)A < (unsigned)B then
-            less <= "1";
-          else
-            less <= "0";
-          end if;
         when "0100" =>
           result <= A and B;
+          less <= "0";
         when "0101" =>
           result <= A or B;
+          less <= "0";
         when "0110" =>
           result <= (A or B) and (not (A and B));
+          less <= "0";
         when "0111" =>
           result <= (not A) and (not B);
+          less <= "0";
         when "1000" =>
           if A < B then
             result <= "11111111111111111111111111111111";
           else 
             result <= "00000000000000000000000000000000";
           end if;
+          less <= "0";
         when "1001" =>
           if (unsigned)A < (unsigned)B then
             result <= "11111111111111111111111111111111";
           else 
             result <= "00000000000000000000000000000000";
           end if;
+          less <= "0";
       end case;
       
     end process;
